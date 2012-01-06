17257996aebfc461460cf81f4c93ce5a50d17111
Ceeram <c33ram@gmail.com>
2012-01-07 00:44:23 +0100

    add option to avoid duplicate emails for duplicate log entries, and clean

--- Lib/Log/Engine/EmailLogger.php
+++ Lib/Log/Engine/EmailLogger.php
@@ -5,20 +5,27 @@ class EmailLogger implements CakeLogInterface {
 
 	public $config = array(
 		'levels' => array('warning', 'notice', 'debug', 'info', 'error'),
-		'email' => 'email_logger'
+		'email' => 'email_logger',
+		'duplicates' => false
 	);
 
     public function __construct($config = array()) {
-		$this->config = array_merge($this->config, $config);
+		$this->config = array_merge($this->config, array('file' => LOGS . 'email_logger.log'), $config);
     }
 
     public function write($type, $message) {
 		if (empty($this->config['levels']) || in_array($type, $this->config['levels'])) {
-			try {
-				CakeEmail::deliver(null, 'EmailLogger: ' . $type, $message, $this->config['email']);
-			} catch(Exception $e) {
-
+			if (!strpos(file_get_contents($this->config['file']), $message)) {
+				try {
+					CakeEmail::deliver(null, 'EmailLogger: ' . $type, $message, $this->config['email']);
+				} catch(Exception $e) {}
 			}
+
+			$output = $message . "\n";
+			file_put_contents($this->config['file'], $output, FILE_APPEND);
+
+		} elseif ($type == 'clean') {
+			file_put_contents($this->config['file'], "");
 		}
     }
 }
